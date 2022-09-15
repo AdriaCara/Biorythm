65f4feb133b2aff8d694134301e48027527ee82e
AdriaCara <adriaProgrammer@gmail.com>
2022-09-15 17:10:57 +0200

    New: stetic.

--- Index.php
+++ Index.php
@@ -7,14 +7,12 @@
         <title>Formulari</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     </head>
-
     <body>
-
-        <nav class="navbar navbar-expand-lg bg-light">
+        <nav class="navbar navbar-expand-lg bg-white">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">Menu</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                    <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
                     <ul class="navbar-nav">
@@ -31,30 +29,25 @@
                 </div>
             </div>
         </nav>
-
-        <div class="container bg-dark" style="padding: 26.7%;">
+        <div class="container bg-white" style="padding: 18.5%;">
             <div class="position-absolute top-50 start-50 translate-middle">
-                <form class="bg-light" action="Resultat.php" method="POST">
+                <form class="bg-white" action="Resultat.php" method="POST">
                     <div class="mb-4">
                         <h2 for="name" class="form-label">Your name: </h2>
-                        <br/>
+                        <br />
                         <input type="text" class="form-label" id="name" name="name">
                     </div>
-                    <br />
                     <div class="mb-4">
-                        <h2 class="" for="age" class="form-label">Your age: </h2>
-                        <br/>
+                        <h2 class="" for="age" class="form-label">Your birth date: </h2>
+                        <br />
                         <input type="date" class="form-label" id="age" name="age">
                     </div>
-                    <button type="submit" class="btn btn-primary end-0">Submit</button>
+                    <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
             </div>
         </div>
-
         <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-            <span class="text-muted">Copyright © 2022 ADRIÀ CARA ROCA & PAU SERRA COMAJOAN</span>
+            <span class="text-muted mx-auto"> Copyright © 2022 ADRIÀ CARA ROCA & PAU SERRA COMAJOAN</span>
         </footer>
-
     </body>
-
 </html>
--- biorritmes.php
+++ biorritmes.php
@@ -1,33 +1,31 @@
 <?php
 
-    class Biorhythm
+class Biorhythm
+{
+
+    public function getDate($date)
     {
 
-        public function getDate($date) 
-        {
+        $today = new DateTime();
 
-            $today = new DateTime();
+        $diff = date_diff($date, $today);
 
-            $diff = date_diff($date, $today);
+        $diff = str_replace("+", "", $diff->format('%R%a'));
 
-            return $diff->format('%R%a');
-
-        }
-
-        public function getPercent($days, $periode) {
-
-            $cicle = $days / $periode;
-
-            $radiants = ((2 * M_PI) * $cicle);
-
-            $sinus = sin($radiants);
-
-            $percent = ((($sinus + 1) * 100) / 2);
-
-            return number_format($percent, 2);
-
-        }
-
+        return $diff;
     }
 
-?>
+    public function getPercent($days, $periode)
+    {
+
+        $cicle = $days / $periode;
+
+        $radiants = ((2 * M_PI) * $cicle);
+
+        $sinus = sin($radiants);
+
+        $percent = ((($sinus + 1) * 100) / 2);
+
+        return number_format($percent, 2);
+    }
+}
