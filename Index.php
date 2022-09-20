632ff5eb14c307b963f8764af4a5efdf7c10959d
AdriaCara <adriaProgrammer@gmail.com>
2022-09-20 22:38:26 +0200

    New: Distribution of the navbar and footer.

--- Index.php
+++ Index.php
@@ -1,54 +1,30 @@
-<!doctype html>
-<html lang="en">
+<?php
 
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Formulari</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    </head>
-    <body>
-        
-        <nav class="navbar navbar-expand-lg bg-white">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Menu</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="Resultat.php">Results</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="Info.php">What's this</a>
-                        </li>
-                    </ul>
-                </div>
+    $activeIndex = "active";
+    $activeResult = "";
+    $activeInfo = "";
+    include 'navbar.php';
+
+?>
+
+<div class="container bg-white" style="padding: 40vh;">
+    <div class="position-absolute top-50 start-50 translate-middle">
+        <form class="bg-white" action="Resultat.php" method="POST">
+            <div class="mb-4">
+                <h2 for="name" class="form-label">Your name: </h2>
+                <br />
+                <input type="text" class="form-label" id="name" name="name">
             </div>
-        </nav>
-        <div class="container bg-white" style="padding: 40vh;">
-            <div class="position-absolute top-50 start-50 translate-middle">
-                <form class="bg-white" action="Resultat.php" method="POST">
-                    <div class="mb-4">
-                        <h2 for="name" class="form-label">Your name: </h2>
-                        <br />
-                        <input type="text" class="form-label" id="name" name="name">
-                    </div>
-                    <div class="mb-4">
-                        <h2 class="" for="age" class="form-label">Your birth date: </h2>
-                        <br />
-                        <input type="date" class="form-label" id="age" name="age">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </form>
+            <div class="mb-4">
+                <h2 class="" for="age" class="form-label">Your birth date: </h2>
+                <br />
+                <input type="date" class="form-label" id="age" name="age">
             </div>
-        </div>
-        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 fixed-botto">
-            <span class="text-muted mx-auto"> Copyright © 2022 ADRIÀ CARA ROCA & PAU SERRA COMAJOAN</span>
-        </footer>
-    </body>
-</html>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</div>
+
+<?php
+    include 'Footer.php';
+?>
--- Info.php
+++ Info.php
@@ -1,45 +1,22 @@
-<!doctype html>
-<html lang="en">
+<?php
 
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Formulari</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    </head>
-    <body>
-        <nav class="navbar navbar-expand-lg bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Menu</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="Index.php">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="Resultat.php">Results</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#">What's this</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <div class="container bg-dark text-center text-light" style="padding: 40vh;">
-            <div class="position-absolute top-50 start-50 translate-middle">
-                <h2>Biorhythm</h2>
-                <br />
-                <p>The biorhythm theory is the pseudoscientific idea that our daily lives are significantly affected by rhythmic cycles with periods of exactly 23, 28 and 33 days, typically a 23-day physical cycle, a 28-day emotional cycle, and a 33-day intellectual cycle. The idea was developed by Wilhelm Fliess in the late 19th century, and was popularized in the United States in the late 1970s. The proposal has been independently tested and, consistently, no validity for it has been found.</p>
-                <br />
-                <p>According to the theory of biorhythms, a person's life is influenced by rhythmic biological cycles that affect his or her ability in various domains, such as mental, physical, and emotional activity. These cycles begin at birth and oscillate in a steady (sine wave) fashion throughout life, and by modeling them mathematically, it is suggested that a person's level of ability in each of these domains can be predicted from day to day. The theory is built on the idea that the biofeedback chemical and hormonal secretion functions within the body could show a sinusoidal behavior over time.</p>
-            </div>
-        </div>
-        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-            <span class="text-muted mx-auto">Copyright © 2022 ADRIÀ CARA ROCA & PAU SERRA COMAJOAN</span>
-        </footer>
-    </body>
-</html>
+    $actveIndex = "";
+    $activeResult = "";
+    $activeInfo = "active";
+    include 'navbar.php';
+
+?>
+
+<div class="container bg-dark text-center text-light" style="padding: 40vh;">
+    <div class="position-absolute top-50 start-50 translate-middle">
+        <h2>Biorhythm</h2>
+        <br />
+        <p>The biorhythm theory is the pseudoscientific idea that our daily lives are significantly affected by rhythmic cycles with periods of exactly 23, 28 and 33 days, typically a 23-day physical cycle, a 28-day emotional cycle, and a 33-day intellectual cycle. The idea was developed by Wilhelm Fliess in the late 19th century, and was popularized in the United States in the late 1970s. The proposal has been independently tested and, consistently, no validity for it has been found.</p>
+        <br />
+        <p>According to the theory of biorhythms, a person's life is influenced by rhythmic biological cycles that affect his or her ability in various domains, such as mental, physical, and emotional activity. These cycles begin at birth and oscillate in a steady (sine wave) fashion throughout life, and by modeling them mathematically, it is suggested that a person's level of ability in each of these domains can be predicted from day to day. The theory is built on the idea that the biofeedback chemical and hormonal secretion functions within the body could show a sinusoidal behavior over time.</p>
+    </div>
+</div>
+
+<?php
+    include 'Footer.php';
+?>
